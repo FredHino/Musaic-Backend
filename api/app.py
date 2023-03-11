f31100348f4e78a438055574a3b1317735c057cc
FredHino <fredhinojosa03@gmail.com>
2023-03-11 15:03:39 -0600

    Update app.py

--- api/app.py
+++ api/app.py
@@ -87,7 +87,7 @@ def main(oauth):
     # for multiple people use
     listofauths = [auth]
     # get genre types?
-    genres = ["oklahoma country"]
+    genres = ["jazz"]
     # "r-n-b", "pop", "jazz", "classical", "alternative", "indie", "j-rock", "latin"
 
     # main playlist

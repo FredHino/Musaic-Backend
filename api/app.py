b957f235ab2b4ce25990ec6336a7d44bd6ad6a4c
FredHino <fredhinojosa03@gmail.com>
2023-03-14 13:33:43 -0400

    jazz to rap

--- api/app.py
+++ api/app.py
@@ -87,7 +87,7 @@ def main(oauth):
     # for multiple people use
     listofauths = [auth]
     # get genre types?
-    genres = ["jazz"]
+    genres = ["rap"]
     # "r-n-b", "pop", "jazz", "classical", "alternative", "indie", "j-rock", "latin"
 
     # main playlist
